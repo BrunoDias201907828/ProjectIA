8581b6fb12146104f4199d228604380b201203da
ptavares00 <prftavares1@gmail.com>
2024-02-24 15:35:18 +0000

    shows ai thinking and improved display dynamics

--- neutreeko_ai.py
+++ neutreeko_ai.py
@@ -1,7 +1,6 @@
 from neutreeko import Neutreeko
 from minmax import minimax
 from minimax_prunning import minimax_pruning
-from mcts2 import montecarlo_treesearch
 import arcade
 import random
 import argparse
@@ -13,20 +12,35 @@ class NeutreekoAI(Neutreeko):
         self.ai_turn = None
         self.mode = mode
         self.ai_fn = {'minimax_pruning': minimax_pruning, 'minimax': minimax}[mode]
+        self.ai_running = None
 
     def setup(self):
         super().setup()
         self.ai_turn = random.choice([True, False])
+        self.ai_running = False
+
+    def on_draw(self):
+        super().on_draw()
+        text = "AI is running..."
+        if self.ai_running:
+            print('here')
+            arcade.draw_text(text, self.width - 150, 50, arcade.color.BLACK, 24, anchor_x="center")
+        arcade.finish_render()
 
     def on_mouse_press(self, x, y, button, key_modifiers) -> None:
         if self.ai_turn:
             self.ai_move()
         self.ai_turn = super().on_mouse_press(x, y, button, key_modifiers)
+        self.on_draw()
         if self.ai_turn:
             self.ai_move()
 
     def ai_move(self):
         print("thinking...")
+        self.ai_running = True
+        self.on_draw()
+        turn_text = f"{self.turn.capitalize()}'s Turn"
+        arcade.draw_text(turn_text, self.width // 2, self.height - 50, arcade.color.BLACK, 24, anchor_x="center")
         _, move, _ = self.ai_fn(set(self.get_white_positions()), set(self.get_black_positions()), self.turn,
                                 played_moves=self.state_counter)
         self.selected_piece = [piece for piece in self.pieces if piece.square.number == move[0]][0]
@@ -35,6 +49,8 @@ class NeutreekoAI(Neutreeko):
         self.ai_turn = False
         self.selected_piece = None
         self.maybe_finish_game()
+        self.ai_running = False
+        self.on_draw()
         print("done")
 
 
@@ -53,6 +69,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-if __name__ == "__main__":
-    main()
