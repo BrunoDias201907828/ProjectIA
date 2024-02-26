1a0310f5d51633665aec365b16ec819cd3130eac
BrunoDias201907828 <BrunoDias201907828@github.com>
2024-02-26 23:14:52 +0000

    heuristics

--- utils.py
+++ utils.py
@@ -66,15 +66,44 @@ def is_winner(positions: tuple | list | set) -> bool:
 
 def eval(node, player, played_moves, first=False):
     depth_penalty = 1 - node.depth / 10
-    value = 0
     if is_winner(getattr(node, player.lower())):
-        value = 2 - depth_penalty  # Later wins are less rewarded
+        return 2 - depth_penalty  # Later wins are less rewarded
     elif is_winner(getattr(node, 'black' if player.capitalize() == 'White' else 'white')):
-        value = -2 + depth_penalty  # Later loses are less penalized
+        return -2 + depth_penalty  # Later loses are less penalized
     elif is_draw(node, played_moves, first):
-        value = -1 + depth_penalty  # Later draws are less penalized
-    return value
+        #-1 + depth_penalty  # Later draws are less penalized
+        return 0
+    return mobility_and_alignment(node)
 
+def mobility_and_alignment(node):
+    next_player = 'White' if node.current_player == 'Black' else 'Black'
+    node2 = Node(white=node.white, black=node.black, depth=node.depth, current_player=next_player)
+    
+    mobility_score = mobility(node) - mobility(node2)
+    alignment_score = alignment_potential(node) - alignment_potential(node2)
+    return 0.3 * mobility_score + 0.7 * alignment_score
+
+def mobility(node):
+    mob = 0
+    for _, moves in possible_moves(node):
+        for _ in moves:
+            mob += 1
+    return mob / 24
+
+def alignment_potential(node):
+    alignment = 0
+    if node.current_player == 'White':
+        pieces = node.white
+    else:
+        pieces = node.black
+    for piece in pieces:
+        for move in possible(piece, pieces):
+            new_set = set(pieces)
+            new_set.remove(piece)
+            new_set.add(move)
+            if is_winner(new_set):
+                alignment += 1
+    return min(alignment / 2, 1)
 
 def is_terminal(node, played_moves, first=False):
     return any([is_winner(node.black), is_winner(node.white)]) or is_draw(node, played_moves, first) or node.depth == 0
