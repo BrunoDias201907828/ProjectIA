faa04ca62b562d3e125f04a400d2ca157d26b995
ptavares00 <prftavares1@gmail.com>
2024-03-07 01:15:33 +0000

    refactor monte_carlo

--- utils_mcts.py
+++ utils_mcts.py
@@ -1,6 +1,5 @@
+from utils import is_draw, is_winner, get_repetitions, possible_moves
 import math
-import numba
-from utils import possible_moves
 
 
 class Node:
@@ -51,20 +50,6 @@ class Node:
         self.children = [child for child in self.children if not child.is_terminal(played_moves)]
 
 
-def get_repetitions(node: Node):
-    key = (frozenset(node.white), frozenset(node.black))
-    repetition = 1
-    parent_node = node.parent
-    while True:
-        if parent_node is None:
-            break
-        if (frozenset(parent_node.white), frozenset(parent_node.black)) == key:
-            repetition = parent_node.repetition + 1
-            break
-        parent_node = parent_node.parent
-    return repetition
-
-
 def perform_action_mcts(node, position, new_position):
     player = node.current_player.lower()
     new_positions = getattr(node, player).copy()
@@ -77,28 +62,6 @@ def perform_action_mcts(node, position, new_position):
     else:
         return Node(white=new_positions, black=node.black.copy(), current_player='Black',
                     repetition=repetition, parent=node)
-
-    
-def is_draw(node, played_moves):
-    key = (frozenset(node.white), frozenset(node.black))
-    repetition = node.repetition + played_moves.get(key, 0)
-    return repetition >= 3
-
-
-@numba.njit
-def _is_winner(positions: tuple) -> bool:
-    xs = [p % 5 for p in positions]
-    ys = [p // 5 for p in positions]
-    dx = {xs[1] - xs[0], xs[2] - xs[1]}
-    dy = {ys[1] - ys[0], ys[2] - ys[1]}
-    if len(dx) == 2 or len(dy) == 2 or dy.pop() > 1 or abs(dx.pop()) > 1:
-        return False
-    return True
-
-
-def is_winner(positions: tuple | list | set) -> bool:
-    positions = sorted(positions)
-    return _is_winner(tuple(positions))
 
 
 def is_terminal(node, played_moves):
